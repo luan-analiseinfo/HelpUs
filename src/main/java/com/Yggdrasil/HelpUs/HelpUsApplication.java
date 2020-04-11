17d2c01344e6ca9f7132eab487ba4bbec285b365
Gabriel Carvalho <gabrielcpcontato@outlook.com>
2020-04-11 10:49:11 -0300

    initial commit

---
+++ src/main/java/com/Yggdrasil/HelpUs/HelpUsApplication.java
@@ -0,0 +1,13 @@
+package com.Yggdrasil.HelpUs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Yggdrasil/HelpUs/HelpUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Yggdrasil.HelpUs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
